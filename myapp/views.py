d6d50c4e0996cc26f145f51877a80659c91069e0
sachitashrestha <sachitashrestha1234@gmail.com>
2023-10-30 22:59:27 +0545

    connecting to database

--- myapp/views.py
+++ myapp/views.py
@@ -1,12 +1,7 @@
 from django.shortcuts import render
 from .models import MyProjectsModel
 
-def index(request): 
-    project = MyProjectsModel()
-    project.id = 1
-    project.name = "Note Project"
-    project.detail = "Effortlessly capture your ideas, tasks, and memories with QuickNote - your sleek, intuitive note-taking companion. Simplify your life."
-    project.url = "https://github.com/PhoenixIgris/NottoApp"
-    project.image = "https://github.com/PhoenixIgris/NottoApp/blob/main/static/images/note.png?raw=true"    
-    context = {"project": project}
-    return render(request, 'index.html', context)
+
+def index(request):
+    myprojects = MyProjectsModel.objects.all()
+    return render(request, 'index.html', {'projects': myprojects})
